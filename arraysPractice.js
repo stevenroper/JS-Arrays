ebb277067a75e6f315163a7a202066677afef622
Steven Roper <steven.m.roper@gmail.com>
2015-01-22 20:43:09 -0700

    complete 11th problem.

--- arraysPractice.js
+++ arraysPractice.js
@@ -186,8 +186,15 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
 //Write a function called addTen that is given 'numbers' as it's only argument and returns a new
 //array after adding ten to each item in numbers. *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
 
-  //Code Here
+var addTen = function (numArray) {
+    var numPlusTenArray = [];
+    for(var i = 0; i < numArray.length; i++) {
+        numPlusTenArray.push(parseInt(numArray[i]) + 10);
+    }
+    return numPlusTenArray;
+};
 
+alert(addTen(numbers));
 
 
 //Next Problem
